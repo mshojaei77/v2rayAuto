309b41211d04ab8bbdc75c3f13e217a99f790706
Mohammad Shojaei <shojaeireal@gmail.com>
2024-02-04 21:28:45 +0330

    Update collector.py

--- collector.py
+++ collector.py
@@ -1,3 +1,134 @@
+subscription_links = [
+    "https://raw.githubusercontent.com/MrMohebi/xray-proxy-grabber-telegram/master/collected-proxies/row-url/actives.txt",
+    "https://raw.githubusercontent.com/soroushmirzaei/telegram-configs-collector/main/splitted/subscribe",
+    "https://raw.githubusercontent.com/Airuop/cross/master/sub/sub_merge.txt",
+    "https://raw.githubusercontent.com/w1770946466/Auto_proxy/main/Long_term_subscription_num",
+    "https://raw.githubusercontent.com/yebekhe/V2Hub/main/merged",
+    "https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/Eternity",
+    "https://raw.githubusercontent.com/yebekhe/TelegramV2rayCollector/main/sub/normal/mix",
+    "https://raw.githubusercontent.com/yebekhe/TelegramV2rayCollector/main/sub/normal/vless",
+    "https://raw.githubusercontent.com/yebekhe/V2Hub/main/Split/Normal/shadowsocks",
+    "https://raw.githubusercontent.com/fakmoth/vmess_tg/main/v2",
+    "https://raw.githubusercontent.com/RaymondHarris971/ssrsub/master/9a075bdee5.txt",
+    "https://raw.githubusercontent.com/satrom/V2SSR/master/SSR/Sub.txt",
+    "https://raw.githubusercontent.com/satrom/V2SSR/master/V2RAY/Sub.txt",
+    "https://raw.githubusercontent.com/barry-far/V2ray-Configs/main/Sub1.txt",
+    "https://raw.githubusercontent.com/v2clash/V2ray-Configs/31f493c584d0c7b56350f4a34d80c53ad82bcc18/Sub9.txt",
+    "https://raw.githubusercontent.com/Saviorhoss/V2ray2/dbb72f8ab0c06fd5bf98d639789b07c818770ee3/hysteria.txt",
+    "https://raw.githubusercontent.com/yyyr-otz/tele-providers-collector/b7a74ee98bcf02c5a507d688a1bed3aa9f7f4ed1/script/raw/protocols/mixed.txt",
+    "https://raw.githubusercontent.com/mahdi-marjani/v2ray-server-regex/f41d1a3cf896b2f1a3af1f508af36ca4361ac983/output.txt",
+    "https://raw.githubusercontent.com/op30mmd/things/909b38024ba14d69dc0f95c238f63bc6e930a00f/eut.txt",
+    "https://raw.githubusercontent.com/Airuop/archive/c3954c9c5155b9c9726f46ef520ae22e29c6ebf1/selected/2311/231113_1936S.txt",
+    "https://raw.githubusercontent.com/Airuop/archive/c3954c9c5155b9c9726f46ef520ae22e29c6ebf1/selected/2311/231127_1045S.txt",
+    "https://raw.githubusercontent.com/ndsphonemy/proxy-sub/f05f42764ced69b9591875fe786dfe230f0ed266/hys-tuic.txt",
+    "https://raw.githubusercontent.com/w1770946466/Auto_proxy/57e88e0b2dec9f04a3eb11b4a57a1a4fc0416c9b/sub/2311/231108.txt",
+    "https://raw.githubusercontent.com/ellondoa/tele-providers-collector/cf1744ead53efd54bc31b16fa16b3eff9ee24498/script/raw/protocols/hysteria.txt",
+    "https://raw.githubusercontent.com/coldwater-10/Vpnclashfa/e7b9b6a2cfb01ce1d94018977eecc06ea0d731dc/raw/tuic%20%26%20hy2.txt",
+    "https://raw.githubusercontent.com/barry-far/V2ray-Configs/ced94ddfff48276ec365b40227561f36b1b64bc8/Sub5.txt",
+    "https://raw.githubusercontent.com/AmirHDevo/temp2/7f15e7ddb311a6f8c5ab338bd1af0e1f2136a84b/configs.txt",
+    "https://raw.githubusercontent.com/MhdiTaheri/V2rayCollector_Py/412f5977c513649827f187ab2385fe725f9f6164/sub/United%20States/config.txt",
+    "https://raw.githubusercontent.com/Crusader-Strike/AKBConf/e092ca19c23eae315e59facf829be930bf08229a/AKBConfigs.txt",
+    "https://raw.githubusercontent.com/M-Mashreghi/free-config-collector/main/Splitted-By-Protocol/ssr.txt",
+    "https://raw.githubusercontent.com/coldwater-10/Vpnclashfa/e7b9b6a2cfb01ce1d94018977eecc06ea0d731dc/raw/tuic%20%26%20hy2.txt",
+    "https://raw.githubusercontent.com/yebekhe/TVC/0f745f92bf381e505f1245abbc7c145b19b11503/config.txt",
+    "https://raw.githubusercontent.com/barry-far/V2ray-Configs/main/Splitted-By-Protocol/vless.txt",
+    "https://raw.githubusercontent.com/w1770946466/Auto_proxy/799bcf41567dff8a3cfea7ab8c2bbbadeffe32ea/sub/2311/231106.txt",
+    "https://raw.githubusercontent.com/yebekhe/TVC/9fac69239c69327fb7a61f5a8d79d107133889e1/lite/config.txt",
+    "https://raw.githubusercontent.com/mrvcoder/V2rayCollector/d5e2e4c2774e527521838841a821cdf9359b4854/ss_iran.txt",
+    "https://raw.githubusercontent.com/freev2rayconfig/V2RAY_SUB/3e6a7786a1601ee3723e8d043ae79e70dbdfd9a8/v2rayconfigs.txt",
+    "https://raw.githubusercontent.com/ndsphonemy/proxy-sub/f8011c8975b97a6be74db81c05f4046f7cf2a804/lt-sub.txt",
+    "https://raw.githubusercontent.com/mahdi-marjani/v2ray-server-regex/f41d1a3cf896b2f1a3af1f508af36ca4361ac983/output.txt",
+    "https://raw.githubusercontent.com/LonUp/NodeList/7b27813bfebb55f4abc982335fa9f14268f2d456/V2RAY/046.txt",
+    "https://raw.githubusercontent.com/deepinor/v2ray_luck/702642c1867e6e5cdf871d91255510c945c40b35/node.txt",
+    "https://raw.githubusercontent.com/Ashkan-m/v2ray/a4c9dcdb41aa71477aabb2588c241c27228163ba/Sub.txt",
+    "https://raw.githubusercontent.com/hkpc/V2ray-Configs/7d9aa0dabf947af180a7dd6302818ed37950531a/Sub1.txt",
+    "https://raw.githubusercontent.com/chxxyz2004/zd/17fca23b73c46b5dd73e9be49b14d219b5e5f836/sub/2401/240110.txt",
+    "https://raw.githubusercontent.com/NiREvil/vless/575cbe54417021787f11fd6618f3c299e2904f25/sub/catnip.txt",
+    "https://raw.githubusercontent.com/v2clash/Autoproxy/a81f840ab2364124133572ec94e4b81fb0d4e58d/sub/2401/240104.txt",
+    "https://raw.githubusercontent.com/dimzon/scaling-sniffle/7f5f4f1c31d96015218da9ead3d07405f3471e46/by-country/EE.txt",
+    "https://raw.githubusercontent.com/Ennzo0/V2ray/e5bf5c5f54a1d8aad71975ccd8e988a3042920b7/all.txt",
+    "https://raw.githubusercontent.com/M450ud/PrList/fc8a69e79d70fdbf955f3ca29a60566051599041/prx.txt",
+    "https://raw.githubusercontent.com/OpenCNLink/freevpn/3cfcff048bfdd1493ccffa52ecf30ce7f9fc6b28/src/list/vmess.txt",
+    "https://raw.githubusercontent.com/shabane/kamaji/75e841a6c9735a4e8d1840cc6c2d95a80b89673b/hub/vmess.txt",
+    "https://raw.githubusercontent.com/w1770946466/Auto_proxy/799bcf41567dff8a3cfea7ab8c2bbbadeffe32ea/sub/2311/231106.txt",
+    "https://raw.githubusercontent.com/Sajad-meow/free-vmess/0a2bf5319c9bec099ebbaa5e0d9bbc8414116880/vp.txt",
+    "https://raw.githubusercontent.com/sunshinehome/zidong/b4a61de307b189c6585a72642d737b0833d8d31e/%E4%BB%A3%E7%90%86%E6%B1%A0%E9%93%BE%E6%8E%A501.txt",
+    "https://raw.githubusercontent.com/hameed-maleki/TVC/89a4522969c57dcc22458cae9898426e5c4f2679/lite/config.txt",
+    "https://raw.githubusercontent.com/xiaozhu2007/xiaozhu2007/61a755a044dbaeea678bc2d1e20f21a21c6d7170/docs/fxxk.txt",
+    "https://raw.githubusercontent.com/Barabama/FreeNodes/4b1deddc866bb4b5e925cac9cd16db5782b59c27/nodes/yudou66.txt",
+    "https://raw.githubusercontent.com/Airuop/archive/f020a942e86d0a0f8e5b1abe53d6348e27b54103/update/2310/231005_0701.txt",
+    "https://raw.githubusercontent.com/Fido6/FreeNodes/57221e59caddde55ffdda21d491ae952cd608049/nodes/halekj.txt",
+    "https://raw.githubusercontent.com/sunshinehome/zidong/b4a61de307b189c6585a72642d737b0833d8d31e/%E7%A7%91%E5%AD%A6%E4%B8%8A%E7%BD%91%E7%9A%84%E6%96%B9%E6%B3%95.txt",
+    "https://raw.githubusercontent.com/rasoulnorouzi/rasoulnorouzi.github.io/26fb7c851f0cafea9e8b1e545c0baa2ce96780a6/sources/files/rey.txt",
+    "https://raw.githubusercontent.com/tristan-deng/v2rayNodesSelected/ba42c865f6f963b8ea1c980207b826c0502eb063/MyNodes.txt",
+    "https://raw.githubusercontent.com/Airuop/archive/f020a942e86d0a0f8e5b1abe53d6348e27b54103/donated/2312/231231_0710M.txt",
+    "https://raw.githubusercontent.com/ResistalProxy/V2Ray/e927ffb37f732edea84f81d1eca555f6b29784c1/server.txt",
+    "https://raw.githubusercontent.com/sunshinehome/zidong/b4a61de307b189c6585a72642d737b0833d8d31e/%E6%88%91%E7%94%A8%E7%9A%84%E4%BB%93%E5%BA%93.txt",
+    "https://raw.githubusercontent.com/resasanian/Mirza/1e45095b374a6e8d3861f1c88f78d51ef59bf61d/mirza-all.txt",
+    "https://raw.githubusercontent.com/Airuop/archive/f020a942e86d0a0f8e5b1abe53d6348e27b54103/update/2310/231005_0503.txt",
+    "https://raw.githubusercontent.com/andytjh/v2ray/0b6e7d606a51a1772e943aa1073f22551ed0f127/07-19v.txt",
+    "https://raw.githubusercontent.com/yyyr-otz/tele-providers-collector/019cb3e07ee52fddbc7caa54fc670c6b9bae6539/sub/proxies.txt",
+    "https://raw.githubusercontent.com/Airuop/archive/f020a942e86d0a0f8e5b1abe53d6348e27b54103/donated/2312/231202_0011M.txt",
+    "https://raw.githubusercontent.com/OpenCNLink/freevpn/3cfcff048bfdd1493ccffa52ecf30ce7f9fc6b28/src/list/vmess.txt",
+    "https://raw.githubusercontent.com/Airuop/archive/6aefdedc2cde21ff91bd14105393cda477c6ae21/donated/2401/240109_1712M.txt",
+    "https://raw.githubusercontent.com/Fido6/FreeNodes/57221e59caddde55ffdda21d491ae952cd608049/nodes/halekj.txt",
+    "https://raw.githubusercontent.com/free18/jd/da18c454eb4ed09466c41ebd24cc6ae214a33e88/07-21f.txt",
+    "https://raw.githubusercontent.com/Airuop/archive/6aefdedc2cde21ff91bd14105393cda477c6ae21/donated/2312/231223_1411M.txt",
+    "https://raw.githubusercontent.com/wudongdefeng/free/914cea8410a6712d45ba8feb88ac2626db2ada0a/nodes/nodefree.txt",
+    "https://raw.githubusercontent.com/sunshinehome/zidong/b4a61de307b189c6585a72642d737b0833d8d31e/%E7%A7%91%E5%AD%A6%E4%B8%8A%E7%BD%91%E7%9A%84%E6%96%B9%E6%B3%95.txt",
+    "https://raw.githubusercontent.com/Sajad-meow/free-vmess/0a2bf5319c9bec099ebbaa5e0d9bbc8414116880/vp.txt",
+    "https://raw.githubusercontent.com/Airuop/archive/6aefdedc2cde21ff91bd14105393cda477c6ae21/donated/2309/230923_2310M.txt",
+    "https://raw.githubusercontent.com/w1770946466/Auto_proxy/799bcf41567dff8a3cfea7ab8c2bbbadeffe32ea/sub/2401/240110.txt",
+    "https://raw.githubusercontent.com/w1770946466/Auto_proxy/799bcf41567dff8a3cfea7ab8c2bbbadeffe32ea/sub/2310/231031.txt",
+    "https://raw.githubusercontent.com/Airuop/archive/6aefdedc2cde21ff91bd14105393cda477c6ae21/donated/2310/231004_0310M.txt",
+    "https://raw.githubusercontent.com/w1770946466/Auto_proxy/799bcf41567dff8a3cfea7ab8c2bbbadeffe32ea/sub/2311/231116.txt",
+    "https://raw.githubusercontent.com/liufung/v2ray-1/5dc697ea84893c770b59c5e2c94d4efda772d94a/Sub.txt",
+    "https://raw.githubusercontent.com/hfarahani/vv/63f6ae6c3f6bdb572c4d5836d72bac9ebf6ea9a4/co.txt",
+    "https://raw.githubusercontent.com/Saviorhoss/V2ray2/dbb72f8ab0c06fd5bf98d639789b07c818770ee3/ss.txt",
+    "https://raw.githubusercontent.com/free18/jd/da18c454eb4ed09466c41ebd24cc6ae214a33e88/07-24p.txt",
+    "https://raw.githubusercontent.com/HDYOU/porxy/a580704fd54db8e7180629d8163d2a8f6158d0dc/data/no_code_proxy.txt",
+    "https://raw.githubusercontent.com/free18/jd/da18c454eb4ed09466c41ebd24cc6ae214a33e88/07-20t.txt",
+    "https://raw.githubusercontent.com/wudongdefeng/free/914cea8410a6712d45ba8feb88ac2626db2ada0a/nodes/yudou66.txt",
+    "https://raw.githubusercontent.com/yyyr-otz/tele-providers-collector/019cb3e07ee52fddbc7caa54fc670c6b9bae6539/sub/proxies.txt",
+    "https://raw.githubusercontent.com/Airuop/archive/6aefdedc2cde21ff91bd14105393cda477c6ae21/donated/2312/231213_1710M.txt",
+    "https://raw.githubusercontent.com/Airuop/archive/6aefdedc2cde21ff91bd14105393cda477c6ae21/donated/2310/231009_1110M.txt",
+    "https://raw.githubusercontent.com/w1770946466/Auto_proxy/799bcf41567dff8a3cfea7ab8c2bbbadeffe32ea/sub/2311/231111.txt",
+    "https://raw.githubusercontent.com/w1770946466/Auto_proxy/799bcf41567dff8a3cfea7ab8c2bbbadeffe32ea/sub/2402/240202.txt",
+    "https://raw.githubusercontent.com/w1770946466/Auto_proxy/799bcf41567dff8a3cfea7ab8c2bbbadeffe32ea/sub/2311/231123.txt",
+    "https://raw.githubusercontent.com/w1770946466/Auto_proxy/799bcf41567dff8a3cfea7ab8c2bbbadeffe32ea/sub/2312/231219.txt",
+    "https://raw.githubusercontent.com/w1770946466/Auto_proxy/799bcf41567dff8a3cfea7ab8c2bbbadeffe32ea/sub/2312/231218.txt",
+    "https://raw.githubusercontent.com/hkpc/Autoproxy/6cf946ddfc677a29062007e4b47d70674474baa7/sub/2401/240110.txt",
+    "https://raw.githubusercontent.com/w1770946466/Auto_proxy/799bcf41567dff8a3cfea7ab8c2bbbadeffe32ea/sub/2312/231216.txt",
+    "https://raw.githubusercontent.com/w1770946466/Auto_proxy/799bcf41567dff8a3cfea7ab8c2bbbadeffe32ea/sub/2401/240122.txt",
+    "https://raw.githubusercontent.com/Airuop/archive/a3a7ed0bcf661d1d503b0005812df1fdc7c6f4b0/donated/2310/231029_0710M.txt",
+    "https://raw.githubusercontent.com/Airuop/archive/a3a7ed0bcf661d1d503b0005812df1fdc7c6f4b0/donated/2310/231013_0508M.txt",
+    "https://raw.githubusercontent.com/liketolivefree/kobabi/195a773fa0ef0525ce6595b47ef9af5f838f2356/sub.txt",
+    "https://raw.githubusercontent.com/sami-soft/v2rayN_proxy/447a8383ec54f243cbeee05196a9093cf60a2ebf/new2.txt",
+    "https://raw.githubusercontent.com/Tazan1230/NoMoreWalls/db6e37cf0b8d5674e02085d371289a71c720a8d1/list_raw.txt",
+    "https://raw.githubusercontent.com/Airuop/archive/a3a7ed0bcf661d1d503b0005812df1fdc7c6f4b0/donated/2310/231004_2210M.txt",
+    "https://raw.githubusercontent.com/Fido6/FreeNodes/57221e59caddde55ffdda21d491ae952cd608049/nodes/kkzui.txt",
+    "https://raw.githubusercontent.com/abbasdvd3/abbasdvd10/73ec60de7a6c6aa6df4c14b511c1da60686891ca/ppo.txt",
+    "https://raw.githubusercontent.com/w1770946466/Auto_proxy/799bcf41567dff8a3cfea7ab8c2bbbadeffe32ea/sub/2311/231124.txt",
+    "https://raw.githubusercontent.com/Airuop/archive/a3a7ed0bcf661d1d503b0005812df1fdc7c6f4b0/donated/2311/231106_0910M.txt",
+    "https://raw.githubusercontent.com/rb360full/V2Ray-Configs/8111c190def6b6714982369cfb1685434eff11cf/Reza-ClashConfig.txt",
+    "https://raw.githubusercontent.com/w1770946466/Auto_proxy/799bcf41567dff8a3cfea7ab8c2bbbadeffe32ea/sub/2401/240109.txt",
+    "https://raw.githubusercontent.com/Airuop/archive/a3a7ed0bcf661d1d503b0005812df1fdc7c6f4b0/donated/2310/231003_2310M.txt",
+    "https://raw.githubusercontent.com/moeinkey/key/b1e8ce539e8328ade2aa79211750f35ce15afe15/new.txt",
+    "https://raw.githubusercontent.com/Airuop/archive/a3a7ed0bcf661d1d503b0005812df1fdc7c6f4b0/donated/2310/231026_0711M.txt",
+    "https://raw.githubusercontent.com/Airuop/archive/a3a7ed0bcf661d1d503b0005812df1fdc7c6f4b0/donated/2401/240111_2111M.txt",
+    "https://raw.githubusercontent.com/Airuop/archive/6aefdedc2cde21ff91bd14105393cda477c6ae21/donated/2311/231122_1510M.txt",
+    "https://raw.githubusercontent.com/Airuop/archive/a3a7ed0bcf661d1d503b0005812df1fdc7c6f4b0/donated/2401/240124_0210M.txt",
+    "https://raw.githubusercontent.com/M450ud/PrList/fc8a69e79d70fdbf955f3ca29a60566051599041/dsl.txt",
+    "https://raw.githubusercontent.com/Fido6/FreeNodes/57221e59caddde55ffdda21d491ae952cd608049/nodes/wenode.txt",
+    "https://raw.githubusercontent.com/rb360full/V2Ray-Configs/8111c190def6b6714982369cfb1685434eff11cf/Reza-configs.txt",
+    "https://raw.githubusercontent.com/Airuop/archive/6aefdedc2cde21ff91bd14105393cda477c6ae21/donated/2309/230916_0501M.txt",
+    "https://raw.githubusercontent.com/Airuop/archive/6aefdedc2cde21ff91bd14105393cda477c6ae21/update/2310/231005_0503.txt",
+    "https://raw.githubusercontent.com/andytjh/v2ray/0b6e7d606a51a1772e943aa1073f22551ed0f127/07-19v.txt",
+    "https://raw.githubusercontent.com/w1770946466/Auto_proxy/799bcf41567dff8a3cfea7ab8c2bbbadeffe32ea/sub/2401/240124.txt",
+    "https://raw.githubusercontent.com/w1770946466/Auto_proxy/799bcf41567dff8a3cfea7ab8c2bbbadeffe32ea/sub/2401/240123.txt",
+    "https://raw.githubusercontent.com/Airuop/archive/6aefdedc2cde21ff91bd14105393cda477c6ae21/donated/2312/231218_0311M.txt",
+    "https://raw.githubusercontent.com/Airuop/archive/a3a7ed0bcf661d1d503b0005812df1fdc7c6f4b0/donated/2310/231018_2211M.txt",
+]
 import urllib.parse
 import requests
 from github import Github
@@ -6,7 +137,7 @@ from collections import OrderedDict
 def push_to_github(file_path, content, repo_name, branch_name, github_token):
     g = Github(github_token)
     repo = g.get_user().get_repo(repo_name)
-    
+
     try:
         file = repo.get_contents(file_path, ref=branch_name)
         repo.update_file(file_path, "Update config file", content, file.sha, branch=branch_name)
@@ -20,10 +151,8 @@ def find_country_code(link):
 
         if fragment:
             decoded_fragment = urllib.parse.unquote(fragment)
-            country_code = decoded_fragment.split('%')[-1][:2]
-
-            if country_code.isalpha() and len(country_code) == 2:
-                return country_code.upper()
+            country_code = decoded_fragment.split('%'or'#')[-1][:2]
+            return country_code.upper()
 
     except Exception as e:
         print(f"Error: {e}")
@@ -36,11 +165,11 @@ def extract_v2ray_configs(subscription_link):
     return config_links
 
 def generate_file_content(subscription_links):
-    file_content = OrderedDict((config_type, set()) for config_type in ["vmess", "ss", "vless", "trojan", "tuic", "hysteria", "hy2", "socks5", "ssr"])
+    file_content = OrderedDict((config_type, set()) for config_type in ["vmess", "ss", "vless", "tuic", "hysteria", "hy2","trojan", "socks5", "ssr"])
     country_mappings = OrderedDict({
-        "country1": {"US", "GB", "CA"},
-        "country2": {"DE", "FR", "NL", "FI"},
-        "country3": {"IR", "RELAY"}
+        "English": {"US", "GB", "CA", "AU", "IE", "NZ"},
+        "Europe": {"DE", "FR", "NL", "FI", "IT", "AL", "TR"},
+        "IRAN": {"IR","RELAY",},
     })
 
     for subscription_link in set(subscription_links):
@@ -55,17 +184,15 @@ def generate_file_content(subscription_links):
 
                 for country_key, country_codes in country_mappings.items():
                     if country_code in country_codes:
+                        # Ensure the country_key is present in file_content
+                        if country_key not in file_content:
+                            file_content[country_key] = set()
                         file_content[country_key].add(config_link)
 
     return file_content
 
-github_token = "your_github_token"  # Replace with your GitHub token
-subscription_links = [
-    "https://raw.githubusercontent.com/MrMohebi/xray-proxy-grabber-telegram/master/collected-proxies/row-url/actives.txt",
-    "https://raw.githubusercontent.com/soroushmirzaei/telegram-configs-collector/main/splitted/subscribe",
-    # ... (add other subscription links)
-]
 
+github_token = "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"  # Replace with your GitHub token
 raw_file_content = generate_file_content(subscription_links)
 
 for config_type, content_set in raw_file_content.items():
@@ -74,3 +201,5 @@ for config_type, content_set in raw_file_content.items():
     branch_name = 'main'
     content = "\n".join(content_set)
     push_to_github(file_path, content, repo_name, branch_name, github_token)
+print("Done !")
+
